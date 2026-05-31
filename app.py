41c614a7cbc3d3a8eec2830882b49fc4475ed069
alexgithub1988 <a.s.egorenkov@gmail.com>
2026-05-31 19:06:01 +0400

    Ñдобавил роутеры и url_for

--- app.py
+++ app.py
@@ -1,17 +1,91 @@
-from fastapi import FastAPI,Request
+from fastapi import FastAPI, Request, APIRouter
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import List
+import uuid
 
+from starlette import status
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/", name="index")
 def index(request: Request):
-    return templates.TemplateResponse({"request": request}, 'index.html')
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {"url_for": request.url_for},
+    )
 
-@app.get("/about")
+@app.get("/about", name="about")
 def about(request: Request):
-    return templates.TemplateResponse({"request": request}, 'about.html')
+    return templates.TemplateResponse(
+        request,
+        "about.html",
+        {"url_for": request.url_for},
+    )
+
+@app.get("/books", name="books")
+def books(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "books.html",
+        {"url_for": request.url_for},
+    )
+
+
+class Book(BaseModel):
+    id: str
+    title: str
+    author: str
+    year: int
+    price: float
+
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    year: int
+    price: float
+
+books_list = [Book(id = str(uuid.uuid4()), title= "Книга 1",author="Автор 1",year = 2003,price = 100.5),
+              Book(id=str(uuid.uuid4()), title="Книга 2", author="Автор 2", year=2004, price=129.5),
+              ]
+
+
+api_router = APIRouter(prefix="/api")
+book_router = APIRouter(prefix="/book",tags=["book"])
+
+@book_router.get("/", response_model=List[Book])
+def get_all_books() -> List[Book]:
+    """
+    Получаем все книги
+
+    """
+    return books_list
+
+@book_router.get("/{id}", response_model=Book)
+def get_book_by_id(id: str) -> Book:
+    """Ищем по ид возвращаем книгу по ид либо говорим что нет такой книги"""
+    for book in books_list:
+        if book.id == id:
+            return book
+    return {"message": "Book not found"}
+
+
+@book_router.post("/", response_model=Book,status_code=status.HTTP_201_CREATED)
+def create_book(book: BookCreate):
+    new_book = Book(id = str(uuid.uuid4()),
+                    title = book.title,
+                    author = book.author,
+                    year = book.year,
+                    price = book.price)
+    books_list.append(new_book)
+    return new_book
+
+api_router.include_router(book_router, tags=["book"])
+app.include_router(api_router)
+
+
 
 
 """
